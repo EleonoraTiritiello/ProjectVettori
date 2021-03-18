c77155aed5287567576c01a27dc04dfbfe7d9800
EleonoraTiritiello <ele99.249@gmail.com>
2021-03-18 14:57:18 +0100

    Project

---
+++ Assets/ShowVariablesComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShowVarible))]
+public class ShowVariablesComponent : Editor
+{
+    void OnSceneGUI()
+    {
+        ShowVarible showVariables = target as ShowVarible;
+
+        showVariables.pointX = Handles.PositionHandle(showVariables.pointX, Quaternion.identity);
+        showVariables.pointY = Handles.PositionHandle(showVariables.pointY, Quaternion.identity);
+        showVariables.TangentA = Handles.PositionHandle(showVariables.TangentA, Quaternion.identity);
+        showVariables.TangentB = Handles.PositionHandle(showVariables.TangentB, Quaternion.identity);
+
+        Handles.DrawBezier(showVariables.pointX, showVariables.pointY, showVariables.TangentA, showVariables.TangentB, Color.blue, null, 5);
+    }
+}
---
+++ Assets/ShowVarible.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ShowVarible : MonoBehaviour
+{
+    public Vector3 pointX;
+    public Vector3 pointY;
+    public Vector3 TangentA;
+    public Vector3 TangentB;
+
+}
